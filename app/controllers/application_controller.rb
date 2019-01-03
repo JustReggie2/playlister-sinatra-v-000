853b9150174eea90d1c2e924f68175919e3cd5a9
Reginald T Carter iI <reginaldcarterii@gmail.com>
2019-01-03 21:49:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,8 @@ class ApplicationController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
   enable :sessions
 
-
-
-
   get '/' do
     erb :index
   end
 
-  # get '/songs' do
-  #   "hello world"
-  #
-  # end
 end
